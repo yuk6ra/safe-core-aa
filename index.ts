217b75a4b2dc420be905326d6adb6d76a46a31d8
yuk6ra <yuki.imamura.all@gmail.com>
2023-05-15 17:52:47 +0900

    Initial commit

---
+++ index.ts
@@ -0,0 +1,18 @@
+require('dotenv').config()
+
+import { ethers } from 'ethers'
+import { EthersAdapter } from '@safe-global/protocol-kit'
+
+// https://chainlist.org/?search=goerli&testnets=true
+const RPC_URL='https://eth-goerli.public.blastapi.io'
+const provider = new ethers.providers.JsonRpcProvider(RPC_URL)
+
+// Initialize signers
+const owner1Signer = new ethers.Wallet(process.env.OWNER_1_PRIVATE_KEY!, provider)
+const owner2Signer = new ethers.Wallet(process.env.OWNER_2_PRIVATE_KEY!, provider)
+const owner3Signer = new ethers.Wallet(process.env.OWNER_3_PRIVATE_KEY!, provider)
+
+const ethAdapterOwner1 = new EthersAdapter({
+  ethers,
+  signerOrProvider: owner1Signer
+})
